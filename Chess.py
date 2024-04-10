47aeddd9c6a836a97136a426a37550254fe9057a
Matt “Kalyp3r” Taylor <mrsymtec@gmail.com>
2024-04-10 18:55:02 +1200

    Corrected and error in the draw_game_over function

--- Chess.py
+++ Chess.py
@@ -359,7 +359,7 @@ def draw_check():
 def draw_game_over():
     pygame.draw.rect(screen, 'black', [200, 200, 400, 70])
     screen.blit(font.render(f'{winner} won the game!!', True, 'white'), (210, 210))
-    screen.blit(font.render(f'Press ENTER to Restart!', True 'white'), (210, 240))
+    screen.blit(font.render)(f'Press ENTER to Restart!', True, 'white'), (210, 240)
 
 # Main game loop
 black_options = check_options(black_pieces, black_locations, 'black')
